37c080f26501d39480fb8bc61b989e0f4a87d9ea
Filip Nikolic <nikolic.filip001@gmail.com>
2023-06-24 21:25:17 +0200

    promena

---
+++ proba.py
@@ -0,0 +1,33 @@
+{
+    'business_status': 'OPERATIONAL', 
+    'geometry': {
+        'location': {'lat': 44.0259971, 'lng': 20.9201717}, 
+        'viewport': {
+            'northeast': {'lat': 44.0273342302915, 'lng': 20.9217336302915}, 
+            'southwest': {'lat': 44.0246362697085, 'lng': 20.9190356697085}
+            }
+        },
+    'icon': 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png', 
+    'icon_background_color': '#909CE1', 
+    'icon_mask_base_uri': 'https://maps.gstatic.com/mapfiles/place_api/icons/v2/hotel_pinlet', 
+    'name': 'Hotel Ženeva Lux', 
+    'opening_hours': {'open_now': True}, 
+    'photos': [
+        {
+            'height': 4000, 
+            'html_attributions': [
+                '<a href="https://maps.google.com/maps/contrib/110774390126121146417">relja kocic</a>'
+                ], 
+            'photo_reference': 'AZose0n35Rm2tIgnezwDxh4W1no_XOk21zRvA3JaFXlR8YzbHXABg0RqO_XVgMH-t-I0kxaniTkq8U6W2k4QyutvfXNneTFQmSKl-aoWaGQjlculldg6DUZY2kqE8mkO4pR-eWNwQUjxd-BvgFwHMToIBUHej67YmoVCYaKiiFrYAJQ7J7_5',
+            'width': 6000
+    }],
+    'place_id': 'ChIJ5yZDK9MmV0cRp9JD0jbWLaY', 
+    'plus_code': {'compound_code': '2WGC+93 Kragujevac, Serbia', 'global_code': '8GP22WGC+93'}, 
+    'price_level': 2, 
+    'rating': 4.5, 
+    'reference': 'ChIJ5yZDK9MmV0cRp9JD0jbWLaY', 
+    'scope': 'GOOGLE', 
+    'types': ['lodging', 'restaurant', 'food', 'point_of_interest', 'establishment'], 
+    'user_ratings_total': 1580, 
+    'vicinity': 'Улица слободе, Крагујевац'
+}
